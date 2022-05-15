58b847674b428c94fafa08e22dfa57b8218cfed9
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-05-15 11:24:40 +0700

    quýquys

--- EX_CRUD/src/Crud.java
+++ EX_CRUD/src/Crud.java
@@ -1,10 +1,13 @@
+import javax.swing.*;
 import java.sql.*;
+import java.util.Scanner;
 
 /*
 Author : Quy Vu
 */
 public class Crud {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         try {
            String url = "jdbc:mysql://localhost:3306/my_music?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC" ;
            String user = "root";
@@ -13,53 +16,91 @@ public class Crud {
            Connection conn = DriverManager.getConnection(url,user,password);
            Statement stm = conn.createStatement();
             /**
-             * Select
+             * Select author
              */
-            System.out.println("Các bài hát của Sơn Tùng\n");
-            String strSelect = "select * from music where author like '%son tung%'";
+            System.out.println("Liệt kê các bài hát của ");
+            String nameMusic = in.nextLine();
+            String strSelect = "select * from music where author like '%"+nameMusic+"%'";
             ResultSet rset = stm.executeQuery(strSelect);
+            ResultSetMetaData rsetMD = rset.getMetaData();
+            int numCol = rsetMD.getColumnCount();
+            for(int i=1;i<=numCol;i++){
+                System.out.printf("%-30s", rsetMD.getColumnName (i));
+            }
+            System.out.println("\n");
+            while (rset.next()) {
+                for(int i=1;i<=numCol;i++){
+                    System.out.printf("%-30s", rset.getString(i));
+                }
+                System.out.println("\n");
+
+            }
+
+            /**
+             * Search music name
+             */
+            System.out.println("\nLiệt kê các bài hát có cụm từ ");
+            String key = in.nextLine();
+            strSelect = "select * from music where name like '%"+key+"%'";
+            rset = stm.executeQuery(strSelect);
             while (rset.next()) {
                 System.out.println(
-                        rset.getInt("id") + ","
-                        + rset.getString("name") + ","
-                        + rset.getString("author") + ","
-                        + rset.getString("year")
+                        rset.getString("name") + ","
+                                + rset.getString("author") + ","
+                                + rset.getString("year")
+                );
+            }
+            /**
+             * Search music ID
+             */
+            System.out.println("\nXem bài hát có ID ");
+            int keyNum = in.nextInt();
+            in.nextLine();
+            strSelect = "select * from music where id=" + keyNum  ;
+            rset = stm.executeQuery(strSelect);
+            while (rset.next()) {
+                System.out.println(
+                        rset.getString("name") + ","
+                                + rset.getString("author") + ","
+                                + rset.getString("year")
                 );
             }
 
             /**
              * Insert
              */
-            String sqlInsert = "Insert into music(name,author,year) values('Em gì ơi','Jack','2020-05-05')";
+            System.out.println("\nThêm dữ liệu");
+            System.out.println("\nMời nhập tên bài hát mới:");
+            String name = in.nextLine();
+            System.out.println("\nMời nhập tên tác giả:");
+            String author = in.nextLine();
+            System.out.println("Mời nhập ngày sáng tác");
+            String date = in.nextLine();
+            String sqlInsert = "Insert into music(name,author,year) values('"+name+"','"+author+"','"+date+"')";
             int countRecord = stm.executeUpdate(sqlInsert);
             System.out.println("\nSố bản ghi được thêm vào bảng = " + countRecord);
 
             /**
-             * Delete
+             * Delete : id
              */
-            String sqlDelete = "delete from music where name='Em gì ơi'";
+            System.out.println("Xóa bài hát có Id");
+            in.nextLine();
+            int id = in.nextInt();
+            String sqlDelete = "delete from music where id=" + id;
             countRecord = stm.executeUpdate(sqlDelete);
             System.out.println("\nSố bản ghi đã bị xóa = " + countRecord);
             /**
-             * Update
+             * Update : id có ngày sx
              */
-            String sqlUpdate = "Update music Set year = '2020-09-02' where name='Em của ngày hôm qua'";
+            System.out.println("Cập nhật bài hát có Id");
+            id = in.nextInt();
+            System.out.println("Nhập ngày sx có dạng yyyy-mm-dd");
+            date  = in.nextLine();
+            String sqlUpdate = "Update music Set year ='"+date+"' where id ="+id;
             countRecord = stm.executeUpdate(sqlUpdate);
             System.out.println("\nSố bản ghi cập nhập thành công = " + countRecord);
 
-            /**
-             * Search music name
-             */
-            System.out.println("\nCác bài hát có cụm từ cơn mưa\n");
-            strSelect = "select * from music where name like '%con mua%'";
-            rset = stm.executeQuery(strSelect);
-            while (rset.next()) {
-                System.out.println(
-                        rset.getString("name") + ","
-                        + rset.getString("author") + ","
-                        + rset.getString("year")
-                );
-            }
+
 
         }catch (SQLException ex){
             ex.printStackTrace();
